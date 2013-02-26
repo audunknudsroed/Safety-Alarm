5c150702391577bfeb56b8ef5f98e75deb4971fd
Audun <audunknudsrod@gmail.com>
2013-02-26 13:30:24 -0800

    First GUI framework for app. Has two activities

--- Safety-Alarm/src/com/application/safety_alarm/MainActivity.java
+++ Safety-Alarm/src/com/application/safety_alarm/MainActivity.java
@@ -1,8 +1,10 @@
 package com.application.safety_alarm;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//populate list
 	}
 
 	@Override
@@ -18,5 +21,8 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
+	public void onClick_new_appointment(View view){
+		 Intent intent = new Intent(this, NewAppointmentActivity.class);
+		 startActivity(intent);
+	}
 }
